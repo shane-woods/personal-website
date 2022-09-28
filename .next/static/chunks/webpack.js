f50de1d70184b86a24b4d2b933fa672b9058b422
Shane <shwoods35@gmail.com>
2022-09-28 13:42:15 -0400

    fixed navbar sizing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d496d402e47bdc1e"; }
+/******/ 		__webpack_require__.h = function() { return "85651627811e8ef9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
